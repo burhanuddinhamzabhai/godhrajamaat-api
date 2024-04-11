dcea6f01625259a5097ae6619f8d417cd4f6b26d
Burhanuddin Bariyawala <burhanuddinhamzabhai@gmail.com>
2024-04-11 14:42:14 +0530

    init

---
+++ main.js
@@ -0,0 +1,20 @@
+const express = require("express");
+var cors = require("cors");
+const dotenv = require("dotenv");
+dotenv.config(); // Load environment variables
+
+const app = express();
+app.use(cors());
+const port = process.env.PORT || 3000;
+
+
+app.set("view engine", "ejs");
+app.use(express.static("public"));
+
+app.listen(port, () => {
+  console.log(`Server listening on port ${port}`);
+});
+
+app.get("/", async (req, res) => {
+    res.render("index.html");
+  });
